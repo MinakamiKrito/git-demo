231927fa73ed2569070da08974d64eddb39588d9
ts <2447383330@qq.com>
2021-11-19 16:02:36 +0800

    second-commit

--- src/main/java/com/renwen/gitdemo/GitTest.java
+++ src/main/java/com/renwen/gitdemo/GitTest.java
@@ -6,5 +6,6 @@ package com.renwen.gitdemo;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello,git");
+        System.out.println("Hello,git2!");
     }
 }
